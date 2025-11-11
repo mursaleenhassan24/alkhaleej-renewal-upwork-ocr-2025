e1b83f77d5f88ccf7c6dc26703992c743201023e
Mursaleen Hassan <mursaleenhassan24@gmail.com>
2025-11-11 17:07:54 +0500

    updated code

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 import uvicorn
 
+from pydantic import BaseModel, Field
+from typing import Optional, List, Union, Dict
+
 from dotenv import load_dotenv
 import os
 
@@ -18,11 +21,25 @@ app.add_middleware(
     allow_headers=["*"], 
 )
 
+class OCR_Model(BaseModel):
+    request_id: str
+    client_name: str
+    phone_number: str
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
 
+
+@app.post("/ocr-processing")
+async def ocr_processing(request: OCR_Model):
+    try:
+        request_id = request.request_id
+        client_name = request.client_name
+        phone_number = request.phone_number
+
+    except Exception as e:
+        print(f"Exception OCR Processing: {e}")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=9001)
